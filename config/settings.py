f23d500ad7cb816a0d26fe3996d4d6cce57219d2
Ibrohim20032 <126556908+GafurovIbrohim@users.noreply.github.com>
2026-03-31 21:19:19 +0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -11,10 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
-import os
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -28,7 +25,8 @@ SECRET_KEY = 'django-insecure-=m^*3q^7-x#xoieieo(m!0uvt%9lbdeaj(6n7m&7dvxj-16@ui
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['your-heroku-app-name.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
+
 
 # Application definition
 
